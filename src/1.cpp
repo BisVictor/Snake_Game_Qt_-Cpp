743e56424781125d41e1d3c7e022d2987db657d4
wartorle <wartorle@student.21-school.ru>
2025-02-22 14:54:21 +0300

    edit

--- src/1.cpp
+++ src/1.cpp
@@ -14,7 +14,10 @@ bool waitHalfSecond() {
 
 typedef struct {
   int** field;
+  int** snake;
   int** next;
+  int* tail_x;
+  int* tail_y;
   int score;
   int high_score;
   int level;
@@ -22,21 +25,83 @@ typedef struct {
   int pause;
 } GameInfo_t;
 
-void screen(GameInfo_t gameInfo) {
+void print_field(GameInfo_t gameInfo) {
+  setlocale(LC_ALL, "");  // Включение поддержки Unicode
   clear();
 
   for (int i = 0; i < FIELD_HEIGHT; i++) {
     for (int j = 0; j < FIELD_WIDTH; j++) {
       if (gameInfo.field[i][j] > 0) {
-        printw("▓▓");
+        printw("##");
       } else {
-        printw("░░");
+        printw("  ");
       }
     }
     printw("\n");
   }
 
   refresh();
+}
+// @brief выделение памяти
+void allocate_memory_for_field(GameInfo_t* gameInfo) {
+  gameInfo->field = new int*[FIELD_HEIGHT];
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    gameInfo->field[i] = new int[FIELD_WIDTH];
+  }
+
+  // Заполнение игрового поля
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    for (int j = 0; j < FIELD_WIDTH; j++) {
+      if (i == 0 || i == FIELD_HEIGHT - 1 || j == 0 || j == FIELD_WIDTH - 1) {
+        gameInfo->field[i][j] = 1;  // Граница
+      } else {
+        gameInfo->field[i][j] = 0;  // Внутреннее поле
+      }
+    }
+  }
+}
+
+void allocate_memory_for_snake(GameInfo_t* gameInfo) {
+  gameInfo->snake = new int*[FIELD_HEIGHT];
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    gameInfo->snake[i] = new int[FIELD_WIDTH];
+  }
+
+  // Заполнение игрового поля
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    for (int j = 0; j < FIELD_WIDTH; j++) {
+      gameInfo->snake[i][j] = 0;  // Внутреннее поле
+    }
+  }
+}
+
+void free_memory_field(GameInfo_t* gameInfo) {
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    delete[] gameInfo->field[i];  // Исправлено: правильное количество строк
+  }
+  delete[] gameInfo->field;
+}
+
+void free_memory_snake(GameInfo_t* gameInfo) {
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    delete[] gameInfo->snake[i];  // Исправлено: правильное количество строк
+  }
+  delete[] gameInfo->snake;
+}
+
+GameInfo_t updateCurrentState(GameInfo_t gameInfo) {
+  GameInfo_t game_info_update;
+  allocate_memory_for_field(&game_info_update);
+  for (int i = 0; i < FIELD_HEIGHT; i++) {
+    for (int j = 0; j < FIELD_WIDTH; j++) {
+      if (gameInfo.field[i][j] > 0 || gameInfo.snake[i][j] > 0) {
+        game_info_update.field[i][j] = 1;
+      } else {
+        game_info_update.field[i][j] = 0;
+      }
+    }
+  }
+  return game_info_update;
 }
 
 int main() {
@@ -50,22 +115,12 @@ int main() {
   gameInfo.speed = 1;
   gameInfo.pause = 0;
 
-  // Выделение памяти для игрового поля
-  gameInfo.field = new int*[FIELD_HEIGHT];
-  for (int i = 0; i < FIELD_HEIGHT; i++) {
-    gameInfo.field[i] = new int[FIELD_WIDTH];
-  }
+  gameInfo.tail_x = new int[FIELD_WIDTH];
+  gameInfo.tail_y = new int[FIELD_HEIGHT];
+  int n_tail = 0;
 
-  // Заполнение игрового поля
-  for (int i = 0; i < FIELD_HEIGHT; i++) {
-    for (int j = 0; j < FIELD_WIDTH; j++) {
-      if (i == 0 || i == FIELD_HEIGHT - 1 || j == 0 || j == FIELD_WIDTH - 1) {
-        gameInfo.field[i][j] = 1;  // Граница
-      } else {
-        gameInfo.field[i][j] = 0;  // Внутреннее поле
-      }
-    }
-  }
+  // Выделение памяти для игрового поля
+  allocate_memory_for_field(&gameInfo);
 
   // Инициализация ncurses
   initscr();
@@ -73,13 +128,16 @@ int main() {
   noecho();
   curs_set(0);
 
-  int i = 10;
+  int i = 13;
   int j = 5;
 
-  while (i >= 0 && waitHalfSecond()) {  // Добавили проверку границы
-    screen(gameInfo);
-    gameInfo.field[i][j] = 1;
+  while (i > 0 && waitHalfSecond()) {  // Добавили проверку границы
+    allocate_memory_for_snake(&gameInfo);
+    gameInfo.snake[i][j] = 1;
+    print_field(updateCurrentState(gameInfo));
+
     i--;
+    free_memory_snake(&gameInfo);
   }
 
   getch();  // Ждем нажатия клавиши перед выходом
@@ -87,10 +145,10 @@ int main() {
   endwin();  // Завершаем ncurses
 
   // Освобождение памяти
-  for (int i = 0; i < FIELD_HEIGHT; i++) {
-    delete[] gameInfo.field[i];  // Исправлено: правильное количество строк
-  }
-  delete[] gameInfo.field;
+  free_memory_field(&gameInfo);
+  free_memory_snake(&gameInfo);
+  delete[] gameInfo.tail_x;
+  delete[] gameInfo.tail_y;
 
   return 0;
 }
